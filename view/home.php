3dd8b85e27c6e73ce4cb50cbbe3da32530fd5558
Cesar COURT <cecourt@e3r5p11.42.fr>
2020-01-27 17:19:49 +0100

    1er push camagru

---
+++ view/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="public/style.css" rel="stylesheet">
+    <title>Camagru - 42</title>
+</head>
+<body>
+    <div class="all">
+        <div class="part-1">
+            <div class="header">
+                <div class="logo">
+                    <div class="logo-in">
+                        <a class="logo-click" href="index.php">
+                            <img src="public/images/camagru.png" alt="logo_camagru" height="30px" width="100px">
+                        </a>
+                    </div>            
+                </div>
+                <div class="search-bar">
+                    <div class="search-bar-in">
+                        <form action="" class="search-all" method="GET">
+                            <input class="search-in" type="text" autocapitalize="none" placehoder="Rechercher" />
+                            <input class="search-btn" type="submit" value="Rechercher" />
+                        </form>
+                    </div>    
+                </div>
+                <div class="buttons">
+                    <div class="buttons-in">
+                        <a class="connect-link" href="login.php">
+                            <button class="connect-in" type="button">Se connecter</button>
+                        </a>
+                        <a class="suscrib-link" href="register.php">S'inscrire</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="part-2">
+        </div>
+        <div class="part-3">
+        </div>
+    </div>
+</body>
+</html>
