212c90f78c926a8f18de982a958b2dec4ea2dab9
CesarCourt <cecourt@student.42.fr>
2020-05-25 16:57:58 +0200

    new start

--- config/database.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-
-$DB_DSN = 'mysql:host=127.0.0.1; dbname=camagru';
-$DB_USER = 'root';
-$DB_PASSWORD = "cecourt";
-
-?>
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require('model/generalModel.php');
-include ('config/database.php');
-
-$redir_path = "tata.php";
-
-echo "<script>setTimeout(\"location.href = \'$redir_path\';\",2000);</script>";
